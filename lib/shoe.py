d753de68d51da6a419fb9fd98a6ded7a32eabf45
shannicec <shanice.cheptoo@student.moringaschool.com>
2024-10-06 18:53:42 +0300

    added the code for book and shoe test for all the tests tp pass

--- lib/shoe.py
+++ lib/shoe.py
@@ -2,3 +2,25 @@
 
 class Shoe:
     pass
+
+
+class Shoe:
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "Old"  # Default condition
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
